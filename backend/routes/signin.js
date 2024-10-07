b85e78fa5acd5122e06b517dbc9059c7a3b04258
Abhishek <s.abhishek7070@gmail.com>
2024-10-07 11:11:32 +0530

    modified

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -10,31 +10,31 @@ const {User} = require('../db')
 
 router.post('/signup', async (req, res) => {
     const body = await req.body
-    const saltRounds = 10; // You can adjust the number of salt rounds
-    const hashedPassword = await bcrypt.hash(body.password, saltRounds)
+    // const saltRounds = 10; // You can adjust the number of salt rounds
+    // const hashedPassword = await bcrypt.hash(body.password, saltRounds)
     const success = User.findOne({email : body.email})
     console.log(success)
     if (!success) {
         
         const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
+            name: body.name,
+            email: body.email,
+            password: body.password
         })
     }
 })
 
 router.post("/signin", async (req, res) => {
     const body = await req.body
-    const success = await User.find({email : body.email})
+    const success = await User.find({email : body.email, password : body.password})
 
-    let isMatch
+    // let isMatch
     
-    if (success) {
-        isMatch = await bcrypt.compare(body.password, success.password);
-    }
+    // if (success) {
+    //     isMatch = await bcrypt.compare(body.password, success.password);
+    // }
 
-    if (success && isMatch) {
+    if (success) {
         const token = jwt.sign(body, secret, {
             expiresIn: 86400
         });
